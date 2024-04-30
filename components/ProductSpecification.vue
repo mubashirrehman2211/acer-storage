882b56134b00ca7478f97369a9bec4b697879131
Mubashir Rehman <malikextra7@gmail.com>
2024-04-30 10:55:59 +0500

    create a new component ProductSpecification

---
+++ components/ProductSpecification.vue
@@ -0,0 +1,81 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="all-center flex-col">
+    <h2 id="specification" class="text-3xl text-[#80c342] font-semibold">SPECIFICATIONS</h2>
+
+    <div class="all-center flex-col my-20 w-[800px]">
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full ">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2"><span>Type</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">DDR5 Desktop Memory</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2">
+          <span>Capacity</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">16 GB / 32 GB</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2">
+          <span>Frequency</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">4800 MHz / 5600 MHz</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2"><span>Timing</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">CL40 / CL46</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2"><span>Pin</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">288 pin</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2"><span>Working Voltage</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">1.1 V</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2">
+          <span>Dimensions</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">133.25 x 31.25 x 3.90 mm</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2"><span>Storage Temperature</span>
+        </div>
+        <div class="w-full text-start justify-start py-4 text-xl mx-3 text-[#777]">-55 °C to 100 °C</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2"><span>Operating Temperature</span>
+        </div>
+        <div class="w-full text-start justify-start py-4 text-xl mx-3 text-[#777]">0 °C to 85 °C</div>
+      </div>
+
+      <div class="flex text-start justify-start border-[1px] border-gray-100 w-full">
+        <div class="all-center border-2 border-[#77bb26] bg-[#77bb26] text-xl text-white w-80 py-2">
+          <span>Warranty</span>
+        </div>
+        <div class="w-full text-start justify-start py-2 text-xl mx-3 text-[#777]">Limited Lifeline</div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
