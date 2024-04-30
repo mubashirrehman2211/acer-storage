21c3c177b1581f8d3150ac9003d31ab5a0ed6d4d
Mubashir Rehman <malikextra7@gmail.com>
2024-04-30 16:37:06 +0500

    Style Support Component

--- components/SupportCard.vue
+++ components/SupportCard.vue
@@ -3,14 +3,8 @@ defineProps(['title'])
 </script>
 
 <template>
-  <div>
-    <div class="border-2 rounded w-[98%] h-[200px] text-start bg-white text-gray-950 p-2 shadow-md my-2">
-      <h3 class="text-lg font-semibold mb-2">{{ title }}</h3>
-      <slot/>
-    </div>
+  <div class="border-2 border-gray-100 rounded w-[30%] text-start bg-white text-gray-950 shadow-md p-3 m-2">
+    <h3 class="text-lg font-semibold mb-2">{{ title }}</h3>
+    <slot/>
   </div>
 </template>
-
-<style scoped>
-
-</style>
