ab2cf6c20a997bf6099e0526eb3795217462433e
Mubashir Rehman <malikextra7@gmail.com>
2024-05-02 12:21:25 +0500

    minor mistake solve in indexCard

--- components/IndexCard.vue
+++ components/IndexCard.vue
@@ -5,7 +5,7 @@ defineProps(['list', 'title', 'imgSrc'])
 <template>
   <div class="all-center flex-col w-[400px] h-[300px] hover:text-[#86d145] cursor-pointer">
     <img :src="imgSrc" alt="" class="w-[70%] p-2 hover:scale-110 duration-200">
-    <h2 class="text-xl px-2">{{ title }}</h2>`
+    <h2 class="text-xl px-2">{{ title }}</h2>
     <ul v-for="item in list" :key="item">
       <li class="p-desc font-light">{{ item }}</li>
     </ul>
